8853c5e3343d53a94fd52b2f5fef69a1d41c8d30
dyhia122 <hammazdyhia@gmail.com>
2026-04-04 18:29:00 +0000

    modification de la structure de la recette

--- Sources/App/Models.swift
+++ Sources/App/Models.swift
@@ -4,6 +4,10 @@ struct Recipe: Codable, Sendable {
     let id: Int64?
     var title: String
     var ingredients: String
+    var missingIngredients: String
     var steps: String
     var category: String
+    var rating: Int
+    var isCooked: Bool
+    var prepTime: Int
 }
